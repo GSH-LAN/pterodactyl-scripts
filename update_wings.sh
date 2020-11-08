b838442d8aafca8fd24907e41df989fadc16c164
danrokzz <zarbock.d@gmail.com>
2020-11-08 17:25:37 +0100

    added working version of update wings script

---
+++ update_wings.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LATEST_VERSION=$(curl -s https://api.github.com/repos/pterodactyl/wings/releases/latest | grep tag_name | cut -d '"' -f 4 | cut -c 2-)
+CURRENT_VERSION=$(/usr/local/bin/wings --version)
+
+if [ "$CURRENT_VERSION" != "$LATEST_VERSION" ]; then
+	curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/download/v$LATEST_VERSION/wings_linux_amd64
+	chmod u+x /usr/local/bin/wings
+	sudo systemctl restart wings
+fi
