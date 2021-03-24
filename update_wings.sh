8dc3a9b324667bc4e6a1b47604c08081e926a768
Maximilian Wittenburg <max.wittenburg@gmail.com>
2021-03-24 12:49:37 +0100

    use sudo for wings wings update

--- update_wings.sh
+++ update_wings.sh
@@ -4,7 +4,7 @@ LATEST_VERSION=$(curl -s https://api.github.com/repos/pterodactyl/wings/releases
 CURRENT_VERSION=$(/usr/local/bin/wings --version)
 
 if [ "$CURRENT_VERSION" != "$LATEST_VERSION" ]; then
-	curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/download/v$LATEST_VERSION/wings_linux_amd64
-	chmod u+x /usr/local/bin/wings
+	sudo curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/download/v$LATEST_VERSION/wings_linux_amd64
+	sudo chmod u+x /usr/local/bin/wings
 	sudo systemctl restart wings
 fi
