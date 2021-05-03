0c8fcdbdadd4a9dcd898307ec74216c53546d89d
danrokzz <zarbock.d@gmail.com>
2021-05-03 16:24:44 +0200

    added @MaxW1337 latest update

--- update_wings.sh
+++ update_wings.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-#LATEST_VERSION=$(curl -s https://api.github.com/repos/pterodactyl/wings/releases/latest | grep tag_name | cut -d '"' -f 4 | cut -c 2-)
 LATEST_VERSION=$(curl -s https://cdn.pterodactyl.io/releases/latest.json | grep wings | cut -d '"' -f 4)
-CURRENT_VERSION=$(/usr/local/bin/wings version)
+CURRENT_VERSION=$(/usr/local/bin/wings version | head -n 1 | cut -d'v' -f2-)
 
-CV1=$(echo $CURRENT_VERSION | awk -F"v" '{ print $2 }')
-CV2=$(echo $CV1 | awk -F" " '{ print $1 }')
-
-CURRENT_VERSION=$(echo $CV2)
 if [ "$CURRENT_VERSION" != "$LATEST_VERSION" ]; then
-	curl -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/download/v$LATEST_VERSION/wings_linux_amd64
-	chmod u+x /usr/local/bin/wings
-	sudo systemctl restart wings
+        echo "Wings is not up to date! installed: $CURRENT_VERSION  available: $LATEST_VERSION"
+        sudo curl -s -S -L -o /usr/local/bin/wings https://github.com/pterodactyl/wings/releases/download/v$LATEST_VERSION/wings_linux_amd64
+        sudo chmod u+x /usr/local/bin/wings
+        sudo systemctl restart wings
 fi
